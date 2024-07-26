ca4f0ec34949ab91e5f95cff99a1e2d644e1e0e9
Alex Leoni <alexxleoni@outlook.com>
2024-07-26 18:10:15 -0300

    exemplo smart tv

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores
+
+    public static void main(String[] args); {
+
+    // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = "";
+    /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+    if(a==b)
+   resultado = "verdadeiro";
+    else
+   resultado = "falso";
+    */
+
+    //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+    }
+}
